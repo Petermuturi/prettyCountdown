d3c1bcb66094fa63ddb1efbe98f6e0eef0857fb0
Petermuturi <pmuturi65@gmail.com>
2016-12-28 17:31:51 +0300

    added countdown functionality and new states

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+// import darkBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import CountDown from './countdown/CountDown'
 
 class App extends Component {
   render() {
+    const muiTheme = getMuiTheme({
+      palette: {
+        textColor: '#FBC02D',
+        primary1Color: '#424242',
+        primary2Color: '#424242'
+      },
+      appBar: {
+        height: 50,
+      },
+    })
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
       <CountDown/>
     </MuiThemeProvider>
     );
--- client/src/countdown/CountDown.js
+++ client/src/countdown/CountDown.js
@@ -13,26 +13,52 @@ export default class CountDown extends Component{
   constructor(props){
     super(props)
     this.state = {
-      day: null
+      day: null,
+      hour: null,
+      minute: null,
+      second: null
     }
   }
   dayChange = (e, date) => {
-    this.setState({
-      day: date
-    });
+
     console.log(moment.utc(new Date(date)).endOf('hour').fromNow())
     let sec_diff = 1000
     let m = (new Date(date) - new Date()) / sec_diff
     let time = parseInt(m.toFixed(0), 10)
     console.log(time);
+    let countdown = time, days, hours, minutes, seconds
+
+    setInterval( ()=> {
+      days = parseInt((countdown / 60 / 60/ 24) % 365, 10)
+      hours = parseInt((countdown / 60 / 60) % 24, 10)
+      minutes = parseInt((countdown / 60) % 60, 10)
+      seconds = parseInt(countdown % 60, 10);
+
+      days = days < 10 ? "0" + days : days;
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      this.setState({
+        day: days,
+        hour: hours,
+        minute: minutes,
+        second: seconds
+      })
+
+      if (--countdown < 0) {
+        countdown = time;
+      }
+    }, 1000)
+
+
   }
   render(){
-
+ console.log(this.state);
     return(
       <div>
         <DatePicker
           hintText="Select Day"
-          name="day.date_set"
           mode="landscape"
           onChange={this.dayChange}
         />
