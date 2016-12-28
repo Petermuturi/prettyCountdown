667f9b2b9007ecb98722e483ec19c725f756aaab
Petermuturi <pmuturi65@gmail.com>
2016-12-28 10:30:40 +0300

    set up timer.js

---
+++ client/src/countdown/CountDown.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Timer from 'timer'
+
+export default class CountDown extends Component{
+  render(){
+
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
