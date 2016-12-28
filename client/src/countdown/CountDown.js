4fae5cebbfb90f93f490167c360f726d4421cecd
Petermuturi <pmuturi65@gmail.com>
2016-12-28 10:45:13 +0300

    set up react-toolbox and initialState

--- client/src/countdown/CountDown.js
+++ client/src/countdown/CountDown.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react'
 import Timer from 'timer'
+import TimePicker from 'react-toolbox/lib/time_picker'
 
 export default class CountDown extends Component{
+  // defining component state
+  constructor(props){
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
   render(){
 
     return(
       <div>
-        
+
       </div>
     )
   }
